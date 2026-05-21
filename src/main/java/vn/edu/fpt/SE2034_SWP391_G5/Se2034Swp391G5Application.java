f78f9f1d8fdfd56cb725651fbdb214f76f472bc4
Nguyễn Việt Dũng <dungnasan126@gmail.com>
2026-05-21 11:50:33 +0700

    Initial project setup

---
+++ src/main/java/vn/edu/fpt/SE2034_SWP391_G5/Se2034Swp391G5Application.java
@@ -0,0 +1,13 @@
+package vn.edu.fpt.SE2034_SWP391_G5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Se2034Swp391G5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Se2034Swp391G5Application.class, args);
+	}
+
+}
---
+++ src/test/java/vn/edu/fpt/SE2034_SWP391_G5/Se2034Swp391G5ApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.fpt.SE2034_SWP391_G5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Se2034Swp391G5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
