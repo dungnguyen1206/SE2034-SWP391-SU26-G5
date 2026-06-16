6e7a04d72db5f7ace5cc241248dabcbd7f50a213
khoalaska <khoa21102311@gmail.com>
2026-06-16 17:24:10 +0700

    add backend logic for account-list

--- src/main/java/vn/edu/fpt/SE2034_SWP391_G5/service/UserService.java
+++ src/main/java/vn/edu/fpt/SE2034_SWP391_G5/service/UserService.java
@@ -1,4 +1,12 @@
 package vn.edu.fpt.SE2034_SWP391_G5.service;
 
+import org.springframework.data.domain.Page;
+import vn.edu.fpt.SE2034_SWP391_G5.dto.response.UserAccountResponse;
+
+import java.util.List;
+
 public interface UserService {
+    Page<UserAccountResponse> getAccountList(String keyword, String roleName, boolean searchFirstName, boolean searchMiddleName, boolean searchLastName, int page, int size);
+    void updateUserRoles(Long userId, List<String> roleNames);
+    void toggleUserStatus(Long userId, String status);
 }
