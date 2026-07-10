a4a36f958d3730e1311908c9cf1927c8724252aa
Hiếu Nguyễn <minhhieu789clup@gmail.com>
2026-07-10 19:58:18 +0700

    service

--- src/main/java/vn/edu/fpt/SE2034_SWP391_G5/repository/MedicalServiceRepository.java
+++ src/main/java/vn/edu/fpt/SE2034_SWP391_G5/repository/MedicalServiceRepository.java
@@ -5,10 +5,12 @@ import org.springframework.stereotype.Repository;
 import vn.edu.fpt.SE2034_SWP391_G5.entity.MedicalService;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface MedicalServiceRepository extends JpaRepository<MedicalService, Long> {
 
     List<MedicalService> findByDepartmentIdAndStatus(Integer departmentId, String status);
     List<MedicalService> findByStatus(String status);
+    Optional<MedicalService> findMedicalServiceById(Long id);
 }
