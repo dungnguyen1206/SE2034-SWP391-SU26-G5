f9ccc0ef2be2fdb6aec28da6c9a3e72c75e327ec
khoalaska <khoa21102311@gmail.com>
2026-07-10 20:27:03 +0700

    fix

--- src/main/java/vn/edu/fpt/SE2034_SWP391_G5/repository/UserRepository.java
+++ src/main/java/vn/edu/fpt/SE2034_SWP391_G5/repository/UserRepository.java
@@ -63,7 +63,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
         Optional<User> findByUsername(String username);
 
-        Optional<User> findByPhone(String phone);
+
 
         boolean existsByPhone(String phone);
 
